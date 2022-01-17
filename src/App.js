2a392d4936222c02a81adc5158f8dda8ec78d29a
Ayon <mirelahi33@gmail.com>
2022-01-17 13:55:30 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles/output.css";
+
+function App() {
+  return (
+    <div className="bg-gray-200 p-20 h-screen flex justify-center items-start">
+      <div>hey testing</div>
+    </div>
+  );
+}
+
+export default App;
